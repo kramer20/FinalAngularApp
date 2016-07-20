baac1df7ef7aec2abed910f9ca507bdc78b54b5f
Malorie Kramer <mkramer2089@gmail.com>
2016-07-20 08:31:25 -0400

    adding favorite pg

---
+++ js/controllers/favoriteControl.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    
+    angular
+    .module('games')
+    .controller('FavoriteController', function(back,$stateParams) {
+       var vm = this;
+
+       		var showInfo = function(){
+           var showGames = back.getGames();
+
+               showGames.then(function(results){
+                  console.log(results);
+                  vm.games = results.data.data;
+               })
+          }       
+        
+    showInfo();
+	      
+    });
+})();
